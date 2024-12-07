cd9b48353cf984729f42024edf43ab8c3eec1612
RohitShah1706 <rlshah03@gmail.com>
2024-12-07 11:26:24 +0530

    test: jenkins

--- cleanup_k8s.sh
+++ cleanup_k8s.sh
@@ -10,11 +10,11 @@ fi
 projectId=$1
 
 # Delete the Kubernetes service and deployment
-kubectl delete deployment "$projectId"
-kubectl delete job "$projectId"
+# kubectl delete deployment "$projectId" -n replit-app
+kubectl delete job "$projectId" -n replit-app
 # ! technically don't need to delete the service & ingress, as the job will delete it
-kubectl delete service "$projectId"
-kubectl delete ingress "$projectId"
+# kubectl delete service "$projectId" -n replit-app
+# kubectl delete ingress "$projectId" -n replit-app
 
 # Check if the commands were successful
 if [ $? -eq 0 ]; then
